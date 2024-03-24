0583a8c4069563204062046a8eb24989dabb8a08
João Bordalo <bordalix@users.noreply.github.com>
2024-03-24 20:11:50 +0000

    bug fix with wss

--- src/lib/boltz.ts
+++ src/lib/boltz.ts
@@ -3,4 +3,4 @@ import { NetworkName } from './network'
 export const getBoltzApiUrl = (network: NetworkName) =>
   network === 'testnet' ? 'https://testnet.boltz.exchange/api' : 'https://api.boltz.exchange'
 
-export const getBoltzWsUrl = (network: NetworkName) => `${getBoltzApiUrl(network).replace('https://', 'ws://')}/v2/ws`
+export const getBoltzWsUrl = (network: NetworkName) => `${getBoltzApiUrl(network).replace('https://', 'wss://')}/v2/ws`
