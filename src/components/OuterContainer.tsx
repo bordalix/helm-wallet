65e17c72426d0c0e9d1e89d9853e9a669c829cf3
João Bordalo <bordalix@users.noreply.github.com>
2024-03-24 20:11:28 +0000

    margin bottom

--- src/components/OuterContainer.tsx
+++ src/components/OuterContainer.tsx
@@ -5,5 +5,5 @@ interface OuterContainerProps {
 }
 
 export default function OuterContainer({ children }: OuterContainerProps) {
-  return <div className='max-w-96 mx-auto h-full p-4 flex flex-col'>{children}</div>
+  return <div className='max-w-96 mx-auto h-full p-4 pb-2 flex flex-col'>{children}</div>
 }
