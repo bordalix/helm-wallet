8f9fc675b2f307229ee81d0611da69aaafb26e71
João Bordalo <bordalix@users.noreply.github.com>
2024-04-18 17:44:52 +0100

    change default explorer to mempool

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 import { ExplorerName } from './explorers'
 import { NetworkName } from './network'
 
-export const defaultExplorer = ExplorerName.Blockstream
+export const defaultExplorer = ExplorerName.Mempool
 export const defaultGapLimit = 5
 export const defaultNetwork = NetworkName.Testnet
 export const someSeconds = 10_000 // 10 seconds
-export const inOneMinute = 60_000
+export const inOneMinute = 60_000 // 60 seconds
