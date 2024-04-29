7a56aacc0b96ac6ff55fcc69bf8321002ba49cfb
João Bordalo <bordalix@users.noreply.github.com>
2024-04-29 22:01:05 +0100

    fix tor icon on dark theme

--- src/icons/Tor.tsx
+++ src/icons/Tor.tsx
@@ -2,15 +2,15 @@ export default function TorIcon() {
   return (
     <svg className='w-8 h-8' viewBox='0 0 192 192' xmlns='http://www.w3.org/2000/svg' fill='none'>
       <path
-        stroke='#000000'
+        stroke='currentColor'
         strokeLinecap='round'
         strokeLinejoin='round'
         strokeWidth='12'
         d='M96 170c40.869 0 74-33.131 74-74 0-40.87-33.131-74-74-74-40.87 0-74 33.13-74 74 0 40.869 33.13 74 74 74Z'
       />
-      <path stroke='#000000' strokeLinecap='round' strokeWidth='12' d='M96 46a50 50 0 1 1 0 100' />
+      <path stroke='currentColor' strokeLinecap='round' strokeWidth='12' d='M96 46a50 50 0 1 1 0 100' />
       <path
-        stroke='#000000'
+        stroke='currentColor'
         strokeLinecap='round'
         strokeWidth='12'
         d='M96 70a26.003 26.003 0 0 1 24.021 16.05 26.012 26.012 0 0 1 0 19.9 26.023 26.023 0 0 1-5.636 8.435A26.015 26.015 0 0 1 96 122'
