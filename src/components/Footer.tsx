520a801102d402d2d400ca4dba7df1427f0db9f1
João Bordalo <bordalix@users.noreply.github.com>
2024-05-04 10:59:31 +0100

    increase height of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
 export default function Footer() {
-  return <p className='text-xs' />
+  return <p className='text-xs'>&nbsp;</p>
   // return <p className='mt-2 text-xs'>Made with 🧡 by @bordalix</p>
 }
