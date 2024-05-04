ba86c00f5693decf531b16adffbfa5bbd02b4b22
João Bordalo <bordalix@users.noreply.github.com>
2024-05-04 11:00:08 +0100

    add onClick to Label

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,9 +1,20 @@
 interface LabelProps {
+  onClick?: () => void
+  pointer?: boolean
   pulse?: boolean
   text: string
+  underline?: boolean
 }
 
-export default function Label({ pulse, text }: LabelProps) {
-  const className = 'block text-sm text-left font-medium mb-1' + (pulse ? ' animate-pulse' : '')
-  return <label className={className}>{text}</label>
+export default function Label({ onClick, pointer, pulse, text, underline }: LabelProps) {
+  const className =
+    'block text-sm text-left font-medium mb-1' +
+    (pulse ? ' animate-pulse' : '') +
+    (pointer ? ' cursor-pointer' : '') +
+    (underline ? ' underline' : '')
+  return (
+    <label className={className} onClick={onClick}>
+      {text}
+    </label>
+  )
 }
