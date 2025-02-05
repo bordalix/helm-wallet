fe17dc8edbbe04c4cb0ffac8bf9d6c60a06e4d09
João Bordalo <bordalix@users.noreply.github.com>
2025-02-05 18:59:35 +0000

    refactor change password to accept biometrics

--- src/screens/Settings/Password.tsx
+++ src/screens/Settings/Password.tsx
@@ -8,27 +8,47 @@ import NewPassword from '../../components/NewPassword'
 import NeedsPassword from '../../components/NeedsPassword'
 import Container from '../../components/Container'
 import { saveMnemonicToStorage } from '../../lib/storage'
+import { WalletContext } from '../../providers/wallet'
+import { isBiometricsSupported, registerUser } from '../../lib/biometrics'
+import Error from '../../components/Error'
+import FingerprintIcon from '../../icons/Fingerprint'
 
 export default function Password() {
   const { toggleShowConfig } = useContext(ConfigContext)
+  const { updateWallet, wallet } = useContext(WalletContext)
 
+  const [error, setError] = useState('')
   const [label, setLabel] = useState('')
   const [mnemonic, setMnemonic] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleProceed = () => {
-    saveMnemonicToStorage(mnemonic, password)
+  const proceed = (pass = password, lockedByBiometrics = true) => {
+    saveMnemonicToStorage(mnemonic, pass)
+    updateWallet({ ...wallet, lockedByBiometrics })
     toggleShowConfig()
   }
+
+  const handleBiometrics = () => {
+    setError('')
+    registerUser()
+      .then(proceed)
+      .catch(() => setError('Biometrics registration failed'))
+  }
+
+  const handleClick = () => proceed(password, false)
 
   return (
     <Container>
       <Content>
         <Title text='Password' subtext='Change your password' />
+        <Error error={Boolean(error)} text={error} />
         {mnemonic ? <NewPassword onNewPassword={setPassword} setLabel={setLabel} /> : null}
       </Content>
       <ButtonsOnBottom>
-        <Button onClick={handleProceed} label={label} disabled={!password} />
+        {isBiometricsSupported() ? (
+          <Button icon={<FingerprintIcon small />} onClick={handleBiometrics} label='Use biometrics' clean />
+        ) : null}
+        <Button onClick={handleClick} label={label} disabled={!password} />
         <Button onClick={toggleShowConfig} label='Back to wallet' secondary />
       </ButtonsOnBottom>
       <NeedsPassword onClose={toggleShowConfig} onMnemonic={setMnemonic} />
