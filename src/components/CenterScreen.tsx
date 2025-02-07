3f30f6dd8c28154fc9726a9a64215c1bbc17b31d
João Bordalo <bordalix@users.noreply.github.com>
2025-02-07 17:22:01 +0000

    make CenterScreen accept onClick

--- src/components/CenterScreen.tsx
+++ src/components/CenterScreen.tsx
@@ -1,6 +1,11 @@
-export default function CenterScreen({ children }: { children: React.ReactNode }) {
+interface CenterScreenProps {
+  children: React.ReactNode
+  onClick?: () => void
+}
+
+export default function CenterScreen({ children, onClick }: CenterScreenProps) {
   return (
-    <div className='flex h-40 text-center'>
+    <div className='flex h-40 text-center' onClick={onClick}>
       <div className='m-auto'>{children}</div>
     </div>
   )
