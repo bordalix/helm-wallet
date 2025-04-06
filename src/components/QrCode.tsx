3918e77c184bfffa10aab9932bb385e55f4789bd
João Bordalo <bordalix@users.noreply.github.com>
2025-04-06 10:48:47 +0100

    use paulmillr/qr to generate qr code

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -1,16 +1,23 @@
 import { formatInvoice } from '../lib/format'
-import QRCode from 'react-qr-code'
+import encodeQR from '@paulmillr/qr'
 
 interface QrCodeProps {
   value: string
 }
 
 export default function QrCode({ value }: QrCodeProps) {
+  // encode value to a gif
+  const qrGifDataUrl = (text: string) => {
+    const gifBytes = encodeQR(text, 'gif', { scale: 7 })
+    const blob = new Blob([gifBytes], { type: 'image/gif' })
+    return URL.createObjectURL(blob)
+  }
+
   return (
-    <div className='w-[300px] mx-auto select-none'>
+    <div className='w-[420px] max-w-full mx-auto select-none'>
       {value ? (
-        <div className='bg-white p-[10px]'>
-          <QRCode size={280} value={value} fgColor='#000000' />
+        <div className='bg-white p-[10px] rounded-md'>
+          <img alt='QR Code' className='w-full' src={qrGifDataUrl(value)} />
         </div>
       ) : null}
       <p className='mt-4'>{formatInvoice(value)}</p>
