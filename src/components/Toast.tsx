31686042d19cbc3726f2ef00eb085dfe61cd4d46
João Bordalo <bordalix@users.noreply.github.com>
2025-04-19 16:00:37 +0100

    remove toast message

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -11,23 +11,6 @@ const showToast = (message: any, duration = 2100) => {
   })
 }
 
-const toastButton = (message: string, onClick: () => void): JSX.Element => (
-  <button className='border font-semibold px-1 py-0.5 rounded-md text-xs' onClick={onClick}>
-    {message}
-  </button>
-)
-
-export const toastNewVersionAvailable = () => {
-  const duration = 21000
-  showToast(
-    <div className='flex gap-4 items-center p-0 text-sm'>
-      <p>New version available</p>
-      {toastButton('Reload', () => window.location.reload())}
-    </div>,
-    duration,
-  )
-}
-
 export const toastCopiedToClipboard = () => {
   showToast(
     <div className='flex items-center p-0 text-sm'>
