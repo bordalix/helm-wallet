827cfb988b5246cfb22ca1a7ce945c515a23d285
João Bordalo <bordalix@users.noreply.github.com>
2025-05-17 09:10:40 +0100

    add console.log

--- src/lib/chainsource.ts
+++ src/lib/chainsource.ts
@@ -59,6 +59,7 @@ export class WsElectrumChainSource implements ChainSource {
   constructor(public explorer: ExplorerName, public network: NetworkName, public tor = false) {
     const wsUrl = getWebSocketExplorerURL(explorer, network, tor)
     if (!wsUrl) throw new Error('Undefined ws url')
+    console.log('connecting to ws:', wsUrl)
     this.ws = new ElectrumWS(wsUrl)
   }
 
